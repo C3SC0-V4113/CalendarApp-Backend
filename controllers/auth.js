b22f47699275903e8d2b3c8f2e6dd70b412e896f
CESCO <cescovalledev@gmail.com>
2023-09-08 22:32:01 -0600

    getting body information and validation for status code and error message

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,34 @@
-const { response } = require("express");
+const { response, request } = require("express");
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = (req = request, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "Invalid username, must be at least 5 characters",
+    });
+  }
   res.json({
     ok: true,
     msg: "register",
+    name,
+    email,
+    password,
   });
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = (req = request, res = response) => {
+  const { email, password } = req.body;
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = (req = request, res = response) => {
   res.json({
     ok: true,
     msg: "renew",
