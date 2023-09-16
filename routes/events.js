a08818ffb637354de74008822f464d61d7994c25
CESCO <cescovalledev@gmail.com>
2023-09-15 23:03:53 -0600

    validation for body in create events

--- routes/events.js
+++ routes/events.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+
 const { validarJWT } = require("../middlewares/validar-jwt");
 const {
   getEventos,
@@ -6,6 +8,8 @@ const {
   actualizarEvento,
   eliminarEvento,
 } = require("../controllers/events");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
 
 const router = Router();
 
@@ -17,7 +21,17 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 /** Crear Evento */
-router.post("/", crearEvento);
+router.post(
+  "/",
+  [
+    /** Middlewares */
+    check("title", "Titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalizacion es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
 
 /** Actualizar Evento */
 router.put("/:id", actualizarEvento);
